1ee252f1880f43e52fc5332c8fbd5c780f3184fa
Waba-tron <wahab_rehman1256@outlook.com>
2023-06-16 18:43:32 +0100

    three hellos

--- src/App.js
+++ src/App.js
@@ -10,6 +10,9 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <h1>Hello world</h1>
+        <h1>Hello world</h1>
+        <h1>Hello world</h1>
+        <h1>Hello world</h1>
                 <a
           className="App-link"
           href="https://reactjs.org"
